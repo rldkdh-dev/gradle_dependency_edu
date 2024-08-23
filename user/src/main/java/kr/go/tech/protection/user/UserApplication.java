303da696a345795de2f07a90a2a632926b2bd6c6
me.jeon <mg.jeon@saycore.io>
2024-08-23 12:41:55 +0900

    Chore: User,Admin Module -- 외장 톰캣 설정

--- user/src/main/java/kr/go/tech/protection/user/UserApplication.java
+++ user/src/main/java/kr/go/tech/protection/user/UserApplication.java
@@ -3,12 +3,18 @@ package kr.go.tech.protection.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserApplication {
+public class UserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UserApplication.class);
+	}
 }
