1f5dda1ae469c6f2a9381ed8e969a45ade78b579
lsw2795 <zoxnzoxn@nate.com>
2024-10-14 14:23:49 +0900

    Feat : 일반회원 수정 API

--- admin/src/main/java/kr/go/tech/protection/admin/domain/account/general/dao/GenMemberDAO.java
+++ admin/src/main/java/kr/go/tech/protection/admin/domain/account/general/dao/GenMemberDAO.java
@@ -1,13 +1,8 @@
 package kr.go.tech.protection.admin.domain.account.general.dao;
 
 import java.util.List;
-import javax.validation.constraints.NotEmpty;
 import kr.go.tech.protection.admin.domain.account.general.dto.GenMemberPO;
 import kr.go.tech.protection.admin.domain.account.general.dto.GenMemberVO;
-import kr.go.tech.protection.admin.domain.account.general.dto.GenMemberVO.DefaultGenMemberVO;
-import kr.go.tech.protection.admin.domain.account.general.dto.GenMemberVO.UpdateRequestVO;
-import kr.go.tech.protection.admin.domain.member.dto.MemberVO;
-import kr.go.tech.protection.admin.domain.member.dto.MemberVO.DefaultMemberVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
