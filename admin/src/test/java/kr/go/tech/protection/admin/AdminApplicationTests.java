caf9fb748c4996557245f4d02d4f52ed20f38efe
me.jeon <mg.jeon@saycore.io>
2024-10-16 14:26:07 +0900

    feat: API Docs -- API 문서화 임시 커밋

--- admin/src/test/java/kr/go/tech/protection/admin/AdminApplicationTests.java
+++ admin/src/test/java/kr/go/tech/protection/admin/AdminApplicationTests.java
@@ -1,13 +1,30 @@
 package kr.go.tech.protection.admin;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationExtension;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
 class AdminApplicationTests {
 
+
+	/*@Test
+	@DisplayName("일반 회원 관리 목록 조회 API.")
+	void selectGenMemberList() throws Exception {
+		System.out.println("test");
+	}*/
+
 	@Test
-	void contextLoads() {
+	@DisplayName("테스트 API")
+	void test() throws Exception {
+		System.out.println("test");
 	}
 
 }
