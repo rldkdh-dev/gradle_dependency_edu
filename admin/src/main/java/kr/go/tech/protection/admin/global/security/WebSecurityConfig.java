5bcfe40477a8506657d01766b7a57c1ca1dc5ff4
aran_93 <aran_93@saycore.io>
2024-10-18 16:24:13 +0900

    Revert "Core : security CORS 설정 추가"
    This reverts commit 31dfd81516057824c20a9e79799361307ffb3b23.

--- admin/src/main/java/kr/go/tech/protection/admin/global/security/WebSecurityConfig.java
+++ admin/src/main/java/kr/go/tech/protection/admin/global/security/WebSecurityConfig.java
@@ -9,9 +9,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -27,8 +24,6 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors
-                        .configurationSource(CorsConfig.corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
